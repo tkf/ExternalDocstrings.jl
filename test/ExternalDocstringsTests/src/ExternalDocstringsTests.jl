56e69537f3d148e740bd693eb72260173caec376
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-20 00:56:26 -0700

    Support <kbd> (#6)

--- test/ExternalDocstringsTests/src/ExternalDocstringsTests.jl
+++ test/ExternalDocstringsTests/src/ExternalDocstringsTests.jl
@@ -1,6 +1,7 @@
 module ExternalDocstringsTests
 
 using ExternalDocstrings
+using ExternalDocstrings: transform_docstring
 using Test
 
 function f end
@@ -15,6 +16,28 @@ end
 function test_readme()
     docstr = string(@doc ExternalDocstringsTests)
     @test occursin("`ExternalDocstringsTests` tests `ExternalDocstrings`", docstr)
+end
+
+function test_kbd()
+    input = """
+    <kbd>Ctrl</kbd>-<kbd>TAB</kbd>
+
+    ```julia
+    julia> str = "<kbd>TAB</kbd>";
+    ```
+    """
+
+    desired = """
+    _Ctrl_-_TAB_
+
+    ```jldoctest LABEL
+    julia> str = "<kbd>TAB</kbd>";
+    ```
+    """
+
+    actual = transform_docstring(input, "LABEL")
+
+    @test Text(actual) == Text(desired)
 end
 
 end  # module ExternalDocstringsTests
